bed7e9f364b0a3f077f1824b67637c9e6372a559
Lisong Xu <xu@unl.edu>
2024-10-06 19:38:04 -0500

    Update PostContentFormatter.java

--- src/unl/soc/PostContentFormatter.java
+++ src/unl/soc/PostContentFormatter.java
@@ -1,5 +1,9 @@
 package unl.soc;
 
 public interface PostContentFormatter {
+	// This method returns a string of exactly 42 characters
+	// the first character is |
+	// then 40 characters
+	// the last character is | 
     String getFormattedContent();
 }
