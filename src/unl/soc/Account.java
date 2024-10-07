a9f3e7453e126dea76c0be1ff4eae6587ee6d7a8
Lisong Xu <xu@unl.edu>
2024-10-07 10:03:39 -0500

    Update Account.java

--- src/unl/soc/Account.java
+++ src/unl/soc/Account.java
@@ -1,10 +1,14 @@
 package unl.soc;
 
-public class Account {
+public class Account implements ContentFormatter{
+    private final String userName;
 
     public String getUsername() {
         return null;
     }
 
-
+    @Override
+    public String getFormattedContent() {
+        return String.format("|  Current user : %-23s|\n", userName);
+    }
 }
